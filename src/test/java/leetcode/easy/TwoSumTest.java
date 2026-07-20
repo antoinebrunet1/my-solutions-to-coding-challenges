9d3f31474f8613724dbc75e20d071dfb2ced6c0f
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-19 20:18:46 -0400

    Fixed formating in example1 test in TwoSumTest

--- src/test/java/leetcode/easy/TwoSumTest.java
+++ src/test/java/leetcode/easy/TwoSumTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class TwoSumTest {
   @Test
   public void example1() {
-    int[] nums = {2,7,11,15};
+    int[] nums = {2, 7, 11, 15};
     int target = 9;
     int[] expectedResult = {0, 1};
     int[] actualResult = TwoSum.twoSum(nums, target);
