482c08131dfd39b499cb1d8d229e297bbf00f9a5
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-20 16:44:02 -0400

    Added ThreeSumTest with example1 test

---
+++ src/test/java/leetcode/medium/ThreeSumTest.java
@@ -0,0 +1,19 @@
+package leetcode.medium;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ThreeSumTest {
+  @Test
+  public void example1() {
+    int[] nums = {-1, 0, 1, 2, -1, -4};
+    List<List<Integer>> expectedResult = List.of(
+        List.of(-1, -1, 2),
+        List.of(-1, 0, 1)
+    );
+    List<List<Integer>> actualResult = ThreeSum.threeSum(nums);
+
+    Assertions.assertEquals(expectedResult, actualResult);
+  }
+}
