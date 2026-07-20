ddfebd45c5ba3596034d2c818a57eff2806c2da3
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-19 20:15:12 -0400

    Added example1 test in leetcode.easy.TwoSumTest

---
+++ src/test/java/leetcode/easy/TwoSumTest.java
@@ -0,0 +1,16 @@
+package leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TwoSumTest {
+  @Test
+  public void example1() {
+    int[] nums = {2,7,11,15};
+    int target = 9;
+    int[] expectedResult = {0, 1};
+    int[] actualResult = TwoSum.twoSum(nums, target);
+
+    assertEquals(expectedResult, actualResult);
+  }
+}
