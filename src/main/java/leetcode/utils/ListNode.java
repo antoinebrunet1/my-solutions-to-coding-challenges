f4c80de81292ef0d69b4b8b894b94fd2392c9daf
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-31 15:20:20 -0400

    Moved getHead and listsAreEqual methods to ListNode class

--- src/main/java/leetcode/utils/ListNode.java
+++ src/main/java/leetcode/utils/ListNode.java
@@ -16,4 +16,48 @@ public class ListNode {
     this.val = val;
     this.next = next;
   }
+
+  public static ListNode getHead(int[] list) {
+    if (list.length == 0) {
+      return null;
+    }
+
+    ListNode head = new ListNode(list[0]);
+    ListNode previousNode = head;
+
+    for (int i = 1; i < list.length; i++) {
+      previousNode.next = new ListNode(list[i]);
+      previousNode = previousNode.next;
+    }
+
+    return head;
+  }
+
+  public static boolean listsAreEqual(ListNode head1, ListNode head2) {
+    if (head1 == null && head2 == null) {
+      return true;
+    }
+
+    if (head1 == null || head2 == null) {
+      return false;
+    }
+
+    ListNode currentNodeOfList1 = head1;
+    ListNode currentNodeOfList2 = head2;
+
+    while (!(currentNodeOfList1 == null && currentNodeOfList2 == null)) {
+      if (currentNodeOfList1 == null || currentNodeOfList2 == null) {
+        return false;
+      }
+
+      if (currentNodeOfList1.val != currentNodeOfList2.val) {
+        return false;
+      }
+
+      currentNodeOfList1 = currentNodeOfList1.next;
+      currentNodeOfList2 = currentNodeOfList2.next;
+    }
+
+    return true;
+  }
 }
--- src/test/java/leetcode/medium/RemoveNthFromEndTest.java
+++ src/test/java/leetcode/medium/RemoveNthFromEndTest.java
@@ -5,55 +5,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RemoveNthFromEndTest {
-  private ListNode getHead(int[] list) {
-    if (list.length == 0) {
-      return null;
-    }
-
-    ListNode head = new ListNode(list[0]);
-    ListNode previousNode = head;
-
-    for (int i = 1; i < list.length; i++) {
-      previousNode.next = new ListNode(list[i]);
-      previousNode = previousNode.next;
-    }
-
-    return head;
-  }
-
-  private boolean listsAreEqual(ListNode head1, ListNode head2) {
-    if (head1 == null && head2 == null) {
-      return true;
-    }
-
-    if (head1 == null || head2 == null) {
-      return false;
-    }
-
-    ListNode currentNodeOfList1 = head1;
-    ListNode currentNodeOfList2 = head2;
-
-    while (!(currentNodeOfList1 == null && currentNodeOfList2 == null)) {
-      if (currentNodeOfList1 == null || currentNodeOfList2 == null) {
-        return false;
-      }
-
-      if (currentNodeOfList1.val != currentNodeOfList2.val) {
-        return false;
-      }
-
-      currentNodeOfList1 = currentNodeOfList1.next;
-      currentNodeOfList2 = currentNodeOfList2.next;
-    }
-
-    return true;
-  }
-
   private void test(int[] input, int n, int[] expectedResultArray) {
-    ListNode expectedResult = getHead(expectedResultArray);
-    ListNode actualResult = new RemoveNthFromEnd().removeNthFromEnd(getHead(input), n);
+    ListNode expectedResult = ListNode.getHead(expectedResultArray);
+    ListNode actualResult = new RemoveNthFromEnd().removeNthFromEnd(ListNode.getHead(input), n);
 
-    Assertions.assertTrue(listsAreEqual(expectedResult, actualResult));
+    Assertions.assertTrue(ListNode.listsAreEqual(expectedResult, actualResult));
   }
 
   @Test
