4b0c2b1cc1a54f069529dd307149b0cfa2f21cef
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-31 17:36:24 -0400

    Added MergeKSortedListsTest class with example1 test

---
+++ src/test/java/leetcode/hard/MergeKSortedListsTest.java
@@ -0,0 +1,19 @@
+package leetcode.hard;
+
+import leetcode.utils.ListNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MergeKSortedListsTest {
+  @Test
+  public void example1() {
+    ListNode list1 = ListNode.getHead(new int[] {1, 4, 5});
+    ListNode list2 = ListNode.getHead(new int[] {1, 3, 4});
+    ListNode list3 = ListNode.getHead(new int[] {2, 6});
+    ListNode[] inputList = new ListNode[] {list1, list2, list3};
+    ListNode expectedResult = ListNode.getHead(new int[] {1, 1, 2, 3, 4, 4, 5, 6});
+    ListNode actualResult = MergeKSortedLists.mergeKLists(inputList);
+
+    Assertions.assertTrue(ListNode.listsAreEqual(expectedResult, actualResult));
+  }
+}
