07cb281d4e86e9b794b3010e65c3966fb8a1bf02
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-02 19:24:19 -0400

    Fixed reverseKGroup method

--- src/main/java/leetcode/hard/ReverseNodesInKGroup.java
+++ src/main/java/leetcode/hard/ReverseNodesInKGroup.java
@@ -10,6 +10,7 @@ public class ReverseNodesInKGroup {
 
     ListNode currentNode = head;
     boolean inGroupOfSizeLessThanK = false;
+    ListNode firstNodeInOldGroup = head;
     ListNode firstNodeInGroup1 = head;
     int groupIndex = 0;
     ListNode result = head;
@@ -20,21 +21,33 @@ public class ReverseNodesInKGroup {
         ListNode oldNext = currentNode.next; // In A -> B, oldNext = B
 
         if (i == 0) {
+          if (groupIndex > 0) {
+            firstNodeInOldGroup = firstNodeInGroup1;
+          }
+
           firstNodeInGroup1 = currentNode;
         }
 
         if (oldNext == null) {
           inGroupOfSizeLessThanK = true;
+          firstNodeInOldGroup.next = reverseKGroup(currentNode, i + 1);
 
           break;
         }
 
-        currentNode.next.next = currentNode; // A -> B becomes A <- B
-        currentNode = oldNext; // In A <- B, currentNode = B
-      }
+        ListNode firstNodeOfGroup2 = currentNode.next.next;
 
-      if (groupIndex == 0) {
-        result = currentNode;
+        currentNode.next.next = currentNode; // A -> B becomes A <- B
+
+        if (groupIndex == 0 && i == k - 2) {
+          result = oldNext;
+        }
+
+        if (i < k - 2) {
+          currentNode = oldNext.next; // In A <- B, currentNode = B
+        } else {
+          currentNode = firstNodeOfGroup2;
+        }
       }
 
       firstNodeInGroup1.next = currentNode.next;
