445c9e98bbcf40412678092a002c7f57a7aa3775
Antoine Brunet <antoinebrunet1@outlook.com>
2026-08-02 22:08:01 -0400

    Added RotateListTest class with example1 test

---
+++ src/test/java/leetcode/medium/RotateListTest.java
@@ -0,0 +1,20 @@
+package leetcode.medium;
+
+import leetcode.utils.ListNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RotateListTest {
+  private void test(int[] head, int k, int[] expected) {
+    ListNode actual = RotateList.rotateRight(ListNode.getHead(head), k);
+
+    Assertions.assertTrue(ListNode.listsAreEqual(ListNode.getHead(expected), actual));
+  }
+
+  @Test
+  public void example1() {
+    int[] head = {1, 2, 3, 4, 5};
+    int k = 2;
+    int[] expected = {4, 5, 1, 2, 3};
+  }
+}
